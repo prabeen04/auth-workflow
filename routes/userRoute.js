93282acddc292a4e1fab16cd1bd51736cb7a9b5a
Prabeen04 <prabeen.strange@gmail.com>
2019-01-26 16:40:19 +0530

    mandrill setup

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const mandrill = require('mandrill-api/mandrill')
+const mandrillClient = new mandrill.Mandrill('fD-HfoAUJEab-XxZb12Q6A')
 const User = require('../models/userModel');
 const saltRound = 10;
 const clientSecret = '!@#$%^&*()_+'
@@ -57,6 +59,96 @@ router.post('/login', function (req, res, next) {
                 .catch(err => { throw err })
         })
         .catch(err => { throw err })
+});
+
+//send mail to the email address
+router.get('/sendMail', function async(req, res, next) {
+    var message = {
+        "html": "<p>Example HTML content</p>",
+        "text": "Example text content",
+        "subject": "example subject",
+        "from_email": "message.from_email@example.com",
+        "from_name": "Example Name",
+        "to": [{
+                "email": "prabeen.strange@gmail.com",
+                "name": "Recipient Name",
+                "type": "to"
+            }],
+        "headers": {
+            "Reply-To": "message.reply@example.com"
+        },
+        "important": false,
+        "track_opens": null,
+        "track_clicks": null,
+        "auto_text": null,
+        "auto_html": null,
+        "inline_css": null,
+        "url_strip_qs": null,
+        "preserve_recipients": null,
+        "view_content_link": null,
+        "bcc_address": "message.bcc_address@example.com",
+        "tracking_domain": null,
+        "signing_domain": null,
+        "return_path_domain": null,
+        "merge": true,
+        "merge_language": "mailchimp",
+        "global_merge_vars": [{
+                "name": "merge1",
+                "content": "merge1 content"
+            }],
+        "merge_vars": [{
+                "rcpt": "recipient.email@example.com",
+                "vars": [{
+                        "name": "merge2",
+                        "content": "merge2 content"
+                    }]
+            }],
+        "tags": [
+            "password-resets"
+        ],
+        "subaccount": "customer-123",
+        "google_analytics_domains": [
+            "example.com"
+        ],
+        "google_analytics_campaign": "message.from_email@example.com",
+        "metadata": {
+            "website": "www.example.com"
+        },
+        "recipient_metadata": [{
+                "rcpt": "recipient.email@example.com",
+                "values": {
+                    "user_id": 123456
+                }
+            }],
+        "attachments": [{
+                "type": "text/plain",
+                "name": "myfile.txt",
+                "content": "ZXhhbXBsZSBmaWxl"
+            }],
+        "images": [{
+                "type": "image/png",
+                "name": "IMAGECID",
+                "content": "ZXhhbXBsZSBmaWxl"
+            }]
+    };
+    var async = false;
+    var ip_pool = "Main Pool";
+    var send_at = new Date();
+    mandrillClient.messages.send({"message": message, "async": async, "ip_pool": ip_pool, "send_at": send_at}, function(result) {
+        console.log(result);
+        /*
+        [{
+                "email": "recipient.email@example.com",
+                "status": "sent",
+                "reject_reason": "hard-bounce",
+                "_id": "abc123abc123abc123abc123abc123"
+            }]
+        */
+    }, function(e) {
+        // Mandrill returns the error as an object with name and message keys
+        console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+        // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+    });
 });
 
 //PUT request to /users/:id
