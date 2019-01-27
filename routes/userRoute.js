4e382d6856c9c79a2ec8e5040a9f9f84961a87d5
Prabeen04 <prabeen.strange@gmail.com>
2019-01-27 20:17:51 +0530

    changeEmail route added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const mandrill = require('mandrill-api/mandrill')
-const mandrillClient = new mandrill.Mandrill('fD-HfoAUJEab-XxZb12Q6A')
+const mandrillClient = new mandrill.Mandrill('zBiwJhM-JAz7wXS0asx-AA')
+// const mandrillClient = new mandrill.Mandrill('fD-HfoAUJEab-XxZb12Q6A')
 const User = require('../models/userModel');
 const saltRound = 10;
 const clientSecret = '!@#$%^&*()_+';
@@ -119,65 +120,13 @@ router.get('/sendMail', function async(req, res, next) {
         }],
         "headers": {
             "Reply-To": "message.reply@example.com"
-        },
-        "important": false,
-        "track_opens": null,
-        "track_clicks": null,
-        "auto_text": null,
-        "auto_html": null,
-        "inline_css": null,
-        "url_strip_qs": null,
-        "preserve_recipients": null,
-        "view_content_link": null,
-        "bcc_address": "message.bcc_address@example.com",
-        "tracking_domain": null,
-        "signing_domain": null,
-        "return_path_domain": null,
-        "merge": true,
-        "merge_language": "mailchimp",
-        "global_merge_vars": [{
-            "name": "merge1",
-            "content": "merge1 content"
-        }],
-        "merge_vars": [{
-            "rcpt": "recipient.email@example.com",
-            "vars": [{
-                "name": "merge2",
-                "content": "merge2 content"
-            }]
-        }],
-        "tags": [
-            "password-resets"
-        ],
-        "subaccount": "customer-123",
-        "google_analytics_domains": [
-            "example.com"
-        ],
-        "google_analytics_campaign": "message.from_email@example.com",
-        "metadata": {
-            "website": "www.example.com"
-        },
-        "recipient_metadata": [{
-            "rcpt": "recipient.email@example.com",
-            "values": {
-                "user_id": 123456
-            }
-        }],
-        "attachments": [{
-            "type": "text/plain",
-            "name": "myfile.txt",
-            "content": "ZXhhbXBsZSBmaWxl"
-        }],
-        "images": [{
-            "type": "image/png",
-            "name": "IMAGECID",
-            "content": "ZXhhbXBsZSBmaWxl"
-        }]
+        }
     };
     var async = false;
     var ip_pool = "Main Pool";
     var send_at = new Date();
     mandrillClient.messages.send({ "message": message, "async": async, "ip_pool": ip_pool, "send_at": send_at }, function (result) {
+        console.log('mail sent');
         console.log(result);
         /*
         [{
@@ -193,5 +142,18 @@ router.get('/sendMail', function async(req, res, next) {
         // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
     });
 });
+
+//change email address after clicking on the changeEmail link sent to the mail
+router.post('/changeEmail', function (req, res, next) {
+    const { _id, email, token } = req.body;
+    User.findOne({ _id })
+        .then(user => {
+            if (!user) return res.status(400).json({ error: 'No user found' })
+            User.findOneAndUpdate({ _id }, { email })
+                .then(user => console.log('after updated', user))
+                .catch(err => console.log('after updated', err))
+        })
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
